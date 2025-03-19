531981f7a0938173f4e0b75ee3c393a20a9f7223
Abhishek31singh <arya31thakur@gmail.com>
2025-03-19 23:06:12 +0530

    Database and other setup is done

---
+++ src/main/java/com/example/demo/model/Contact.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
+
