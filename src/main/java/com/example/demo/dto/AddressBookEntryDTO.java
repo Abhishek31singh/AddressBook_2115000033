40a3a0d85468cfec5ad30c01b27b2e492983dded
Abhishek31singh <arya31thakur@gmail.com>
2025-03-19 23:28:55 +0530

    Dto and model are implemented

---
+++ src/main/java/com/example/demo/dto/AddressBookEntryDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class AddressBookEntryDTO {
+
+    private Long id; // Optional, only for responses
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Phone number cannot be empty")
+    private String phone;
+}
+
