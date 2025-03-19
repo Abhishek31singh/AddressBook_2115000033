f8caa4e91034d7930f3a7dca9cd68b43949a6d0f
Abhishek31singh <arya31thakur@gmail.com>
2025-03-20 00:06:19 +0530

    JWToken Authentication is implemented

---
+++ src/main/java/com/example/demo/dto/AuthUserDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+public class AuthUserDTO {
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    // Getters and Setters
+}
